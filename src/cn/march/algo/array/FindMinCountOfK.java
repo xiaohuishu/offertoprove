93fe743d7101edd6ee42a63b7af65ef48086d0ff
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-17 15:50:56 +0800

    update cn.march.algo.array.FindMinCountOfK.java update makeHeap methid

--- src/cn/march/algo/array/FindMinCountOfK.java
+++ src/cn/march/algo/array/FindMinCountOfK.java
@@ -1,107 +1,191 @@
-
-
-
-package cn.march.algo.array;
-
-import cn.march.algo.sort.QuickSort;
-
-
-/**
- * 问题描述：
- * 		给定一个数量为n的数组(int)
- * 		输出最小的k个数
- * 
- * @author antsmarth
- *
- */
-public class FindMinCountOfK {
-
-	//测试方法
-	public static void main(String[] args) {
-		
-		int[] arrays = new int[]{6, 8, 12, 4, 7, 9, 15, 2};
-		
-		findMinCountOfKByQuickSort(arrays, 3, arrays.length);
-		
-		findMinCountOfKByNonSort(arrays, 3, arrays.length);
-		
-	}
+	
+	
+	
+	package cn.march.algo.array;
+	
+	import cn.march.algo.sort.QuickSort;
+	
 	
 	/**
+	 * 问题描述：
+	 * 		给定一个数量为n的数组(int)
+	 * 		输出最小的k个数
 	 * 
-	 * 最直观的思路：
-	 * 		先对数组进行排序
-	 * 	 	输出前k个数 
-	 * 
-	 * @param arrays
-	 * @param k
-	 * @param length
+	 * @author antsmarth
+	 *
 	 */
-	private static void findMinCountOfKByQuickSort(int [] arrays, int k, int length) {
-		
-		if(k <= 0 || length == 1)
-			throw new RuntimeException("设置的参数不合法!");
-		
-		//快速排序
-		QuickSort.quickSort(arrays, 0, length-1);
-		
-		System.out.println("通过排序数组方式：arrays数组中最小的" + k + "个数: ");
-		
-		for(int i = 0; i < k; i++)
-			System.out.print(arrays[i] + " ");
-		
-		System.out.println();
-		
-	}
+	public class FindMinCountOfK {
 	
-	/**
-	 * 基本思路：
-	 * 		首先创建一个长度为k的数组k_arrays;		
-	 * 		将arrays前k个数赋值给k_arrays
-	 * 		从k位置开始开始遍历arrays：
-	 * 			先获取k_arrays中最大的数:k_array_max;
-	 * 			将k_array_max与arrays[k...length-1]比较
-	 * 			若k_array_max > arrays[k...length-1]
-	 * 				进行交换值,将arrays[i]的值赋给k_arrays[j];
-	 * 			否则继续遍历比较
-	 * 
-	 * @param arrays
-	 * @param k
-	 * @param length
-	 */
-	private static void findMinCountOfKByNonSort(int [] arrays, int k, int length) {
-		
-		//边界处理
-		if(k <= 0 || length == 1)
-			throw new RuntimeException("设置的参数不合法!");
-		
-		int[] k_arrays = new int[k];
-		
-		//初始化k_arrays;
-		for(int i = 0; i < k; i++)
-			k_arrays[i] = arrays[i];
-		
-		int k_array_max = k_arrays[0];
-		
-		//从k起始位置开始遍历arrays;
-		for(int i = k; i < length; i++) {
+		//测试方法
+		public static void main(String[] args) {
 			
-			int j = 0;
-			for(j = 1; j < k; j++) 	
-				if(k_array_max < k_arrays[j])
-					k_array_max = k_arrays[j];
+			int[] arrays = new int[]{6, 8, 12, 4, 7, 9, 15, 2};
 			
-			if(k_array_max > arrays[i])
-				k_arrays[j] = arrays[i];
+			findMinCountOfKByQuickSort(arrays, 3, arrays.length);
+			
+			findMinCountOfKByNonSort(arrays, 3, arrays.length);
+			
+			findMinCountOfKByMakeHeap(arrays, 3, arrays.length);
 			
 		}
 		
-		System.out.println("通过不排序数组方式：arrays数组中最小的" + k + "个数: ");
+		/**
+		 * 
+		 * 最直观的思路：
+		 * 		先对数组进行排序
+		 * 	 	输出前k个数 
+		 * 
+		 * @param arrays
+		 * @param k
+		 * @param length
+		 */
+		private static void findMinCountOfKByQuickSort(int [] arrays, int k, int length) {
+			
+			if(k <= 0 || length <= 0)
+				throw new RuntimeException("设置的参数不合法!");
+			
+			if(length == 1) {
+				
+				System.out.println("最小的一个数：" + arrays[0]);
+				return ;
+			
+			}
+			
+			//快速排序
+			QuickSort.quickSort(arrays, 0, length-1);
+			
+			System.out.println("通过排序数组方式：arrays数组中最小的" + k + "个数: ");
+			
+			for(int i = 0; i < k; i++)
+				System.out.print(arrays[i] + " ");
+			
+			System.out.println();
+			
+		}
 		
-		for(int i = 0; i < k; i++)
-			System.out.print(k_arrays[i] + " ");
+		/**
+		 * 基本思路：
+		 * 		首先创建一个长度为k的数组k_arrays;		
+		 * 		将arrays前k个数赋值给k_arrays
+		 * 		从k位置开始开始遍历arrays：
+		 * 			先获取k_arrays中最大的数:k_array_max;
+		 * 			将k_array_max与arrays[k...length-1]比较
+		 * 			若k_array_max > arrays[k...length-1]
+		 * 				进行交换值,将arrays[i]的值赋给k_arrays[j];
+		 * 			否则继续遍历比较
+		 * 
+		 * @param arrays
+		 * @param k
+		 * @param length
+		 */
+		private static void findMinCountOfKByNonSort(int [] arrays, int k, int length) {
+			
+			//边界处理
+			if(k <= 0 || length <= 0)
+				throw new RuntimeException("设置的参数不合法!");
+			
+			if(length == 1) {
+				
+				System.out.println("最小的一个数：" + arrays[0]);
+				return ;
+			
+			}
+			
+			int[] k_arrays = new int[k];
+			
+			//初始化k_arrays;
+			for(int i = 0; i < k; i++)
+				k_arrays[i] = arrays[i];
+			
+			int k_array_max = k_arrays[0];
+			
+			//从k起始位置开始遍历arrays;
+			for(int i = k; i < length; i++) {
+				
+				int j = 0;
+				for(j = 1; j < k; j++) 	
+					if(k_array_max < k_arrays[j])
+						k_array_max = k_arrays[j];
+				
+				if(k_array_max > arrays[i])
+					k_arrays[j] = arrays[i];
+				
+			}
+			
+			System.out.println("通过不排序数组方式：arrays数组中最小的" + k + "个数: ");
+			
+			for(int i = 0; i < k; i++)
+				System.out.print(k_arrays[i] + " ");
+			
+			System.out.println();
+			
+		}
 		
-		System.out.println();
+		private static void findMinCountOfKByMakeHeap(int[] arrays, int k, int length) {
+			
+			if(k <=0 || length <= 0)
+				throw new RuntimeException("设置的参数不合法!");
+			
+			if(length == 1) {
+				
+				System.out.println("最小的一个数：" + arrays[0]);
+				return ;
+			
+			}
+			
+			int[] k_arrays = new int[k];
+			
+			for(int i = 0; i < k; i++)
+				k_arrays[i] = arrays[i];
+			
+			for(int i = k; i < length; i++) {
+				
+				makeHeap(k_arrays, k);
+				
+				if(k_arrays[k-1] > arrays[i])
+					k_arrays[k-1] = arrays[i];
+				
+			}
+			
+			System.out.println("通过不排序数组方式(建立最大堆)：arrays数组中最小的" + k + "个数: ");
+			
+			for(int i = 0; i < k; i++)
+				System.out.print(k_arrays[i] + " ");
+			
+			System.out.println();
+					
+		}
+		
+		private static void makeHeap(int[] k_arrays, int length) {
+			
+			if(length <= 0 || k_arrays == null)
+				throw new RuntimeException("设置参数不合法!");
+			
+			if(length == 1)
+				return ;
+			
+			int i = 0;
+			
+			int temp = k_arrays[i];
+			
+			int j = 2 * i + 1;
+			
+			while(j < length) {
+				
+				if( j+1 < length && k_arrays[j] > k_arrays[j+1])
+					j++;
+				
+				if(k_arrays[j] >= temp)
+					break;
+				
+				k_arrays[i] = k_arrays[j];
+				i = j;
+				j = 2 * i + 1; 
+				
+			}
+			
+			k_arrays[i] = temp;
+			
+		}
 		
 	}
-}
