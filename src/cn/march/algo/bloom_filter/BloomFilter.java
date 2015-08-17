0dd6a06a9e4634bf53799f1d5e9195c31c3bf1a5
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-17 10:41:07 +0800

    push maxProductSubstring

--- src/cn/march/algo/bloom_filter/BloomFilter.java
+++ src/cn/march/algo/bloom_filter/BloomFilter.java
@@ -1,119 +1,123 @@
-package cn.march.algo.bloom_filter;
-
-
-import java.util.*;
-
-public class BloomFilter {
-
-    //布隆过滤器的比特长度
-    private static final int DEFAULT_SIZE = 2<<24; 
 	
 
-    //选取质数，能很好的降低错误率
-    private static final int[] seeds = {3,5,7,11,13,31,37,61};
 
-    private static BitSet bits = new BitSet(DEFAULT_SIZE);
+	package cn.march.algo.bloom_filter;
 	
-    //初始化一个哈希类数组
-    private static SimpleHash[] func = new SimpleHash[seeds.length];
-
-    /**
-     * 向BitSet添加一个字符串
-     * @param value
-     */
-    private static void addValue(String value) {
-
-        for(SimpleHash fun : func) {
-
-            bits.set(fun.hash(value), true);
-
-        }
-
-    }
-
-    
-    public static void add(String value) {
-        
-        if(value != null)
-            addValue(value);
-
-    }
-
-    //判断一个字符串是否在BitSet中是否存在
-    public static boolean contains(String value) {
-        
-        if(value == null) 
-            return false;
-
-        boolean ret = true;
-
-        for(SimpleHash f : func)
-            ret = ret && bits.get(f.hash(value));
-
-        return ret;
-
-    }
-
-
-    public static void main(String [] args) {
-        
-        String value = "shuxiaohui1105@gmail.com";
-
-        for(int i = 0; i < seeds.length; i++) {
-
-            func[i] = new SimpleHash(DEFAULT_SIZE, seeds[i]);
-
-        }
-
-        add(value);
-
-        System.out.println(contains(value));
-
-    }
-
-
-
-    /**
-     * 哈希类：
-     * 	 	一个cap容量(2 << 24),即BitSet容量(很大)和选定的一个质数
-     * 		通过对应的String value字符串来计算一个对应的hash值
-     * 
-     * @author antsmarth
-     *
-     */
-    static class SimpleHash {
-      
-    	//容量(DEFAULT_SIZE)
-        private int cap;
-        //质数
-        private int seed;
-
-        public SimpleHash(int cap, int seed) {
-            
-            this.cap = cap;
-            this.seed = seed;
-
-        }
-
-        //计算hash值
-        public int hash(String value) {
-            
-            int result = 0;
-
-            int len = value.length();
-
-            for(int i = 0; i < len; i++) {
-                
-                result = seed * result + value.charAt(i);
-
-            }
-
-            return (cap - 1) & result;
-
-        }
-
-
-    }
-
-
-}
+	
+	
+	import java.util.*;
+	
+	public class BloomFilter {
+	
+	    //布隆过滤器的比特长度
+	    private static final int DEFAULT_SIZE = 2<<24; 
+		
+	
+	    //选取质数，能很好的降低错误率
+	    private static final int[] seeds = {3,5,7,11,13,31,37,61};
+	
+	    private static BitSet bits = new BitSet(DEFAULT_SIZE);
+		
+	    //初始化一个哈希类数组
+	    private static SimpleHash[] func = new SimpleHash[seeds.length];
+	
+	    /**
+	     * 向BitSet添加一个字符串
+	     * @param value
+	     */
+	    private static void addValue(String value) {
+	
+	        for(SimpleHash fun : func) {
+	
+	            bits.set(fun.hash(value), true);
+	
+	        }
+	
+	    }
+	
+	    
+	    public static void add(String value) {
+	        
+	        if(value != null)
+	            addValue(value);
+	
+	    }
+	
+	    //判断一个字符串是否在BitSet中是否存在
+	    public static boolean contains(String value) {
+	        
+	        if(value == null) 
+	            return false;
+	
+	        boolean ret = true;
+	
+	        for(SimpleHash f : func)
+	            ret = ret && bits.get(f.hash(value));
+	
+	        return ret;
+	
+	    }
+	
+	
+	    public static void main(String [] args) {
+	        
+	        String value = "shuxiaohui1105@gmail.com";
+	
+	        for(int i = 0; i < seeds.length; i++) {
+	
+	            func[i] = new SimpleHash(DEFAULT_SIZE, seeds[i]);
+	
+	        }
+	
+	        add(value);
+	
+	        System.out.println(contains(value));
+	
+	    }
+	
+	
+	
+	    /**
+	     * 哈希类：
+	     * 	 	一个cap容量(2 << 24),即BitSet容量(很大)和选定的一个质数
+	     * 		通过对应的String value字符串来计算一个对应的hash值
+	     * 
+	     * @author antsmarth
+	     *
+	     */
+	    static class SimpleHash {
+	      
+	    	//容量(DEFAULT_SIZE)
+	        private int cap;
+	        //质数
+	        private int seed;
+	
+	        public SimpleHash(int cap, int seed) {
+	            
+	            this.cap = cap;
+	            this.seed = seed;
+	
+	        }
+	
+	        //计算hash值
+	        public int hash(String value) {
+	            
+	            int result = 0;
+	
+	            int len = value.length();
+	
+	            for(int i = 0; i < len; i++) {
+	                
+	                result = seed * result + value.charAt(i);
+	
+	            }
+	
+	            return (cap - 1) & result;
+	
+	        }
+	
+	
+	    }
+	
+	
+	}
---
+++ src/cn/march/algo/dp/maxProductSubstring.java
@@ -0,0 +1,86 @@
+	
+
+
+	package cn.march.algo.dp;
+	
+	
+	
+	/**
+	 * 问题描述： 
+	 * 		给定一个序列：{-2.5, 4, 0, 3, 0.5, 8, -1} 
+	 * 		
+	 * 		取最大乘积连续字串的值 
+	 * 		比如在上述例子中：最大乘积为 3 * 0.5 * 8
+	 * 
+	 * @author antsmarth
+	 *
+	 */
+	public class maxProductSubstring {
+	
+		/**
+		 * 传统的方式解决,通过两次循环的方式来计算;
+		 * @param queues
+		 * @param length
+		 * @return
+		 */
+		private double maxProductSubstringOfNormal(double[] queues, int length) {
+	
+			double maxResult = queues[0];
+	
+			for (int i = 0; i < length; i++) {
+	
+				double temp = 1;
+	
+				for (int j = i; j < length; j++) {
+	
+					temp *= queues[j];
+	
+					if (temp > maxResult)
+						maxResult = temp;
+					
+				}
+	
+			}
+	
+			return maxResult;
+	
+		}
+	
+		/**
+		 * 利用动态规划来解决：
+		 * 	首先构造状态方程：
+		 * 		max_end = Math.max(Math.max(max_end * queues[i], min_end * queues[i]), queues[i]);
+		 * 		min_end = Math.min(Math.min(min_end * queues[i], max_end * queues[i]), queues[i]);
+		 * 
+		 * 	基本思路：
+		 * 		在数组中找到一个子序列,使得它乘积最大
+		 * 		同时找到一个子序列,使得它乘积最小
+		 *		以max_end表示以queues[i]结尾的最大乘积
+		 *		以min_end表示以queues[i]结尾的最小乘积 
+		 * 
+		 * @param queues
+		 * @param length
+		 * @return
+		 */
+		private double maxProductSubstringOfDp(double[] queues, int length) {
+	
+			double maxEnd = queues[0];
+			double minEnd = queues[0];
+			double maxResult = queues[0];
+	
+			for (int i = 0; i < length; i++) {
+	
+				double end_max = maxEnd * queues[i];
+				double end_min = maxEnd * queues[i];
+	
+				maxEnd = Math.max(Math.max(end_max, end_min), queues[i]);
+				minEnd = Math.min(Math.min(end_max, end_min), queues[i]);
+	
+				maxResult = Math.max(maxEnd, maxResult);
+	
+			}
+	
+			return maxResult;
+		}
+	
+	}
