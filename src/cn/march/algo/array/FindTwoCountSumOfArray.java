2e2f29b0eb69d946764a18b3d067b2d0d5a125ed
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-18 09:38:28 +0800

    update cn.march.algo.array.FindTwoCountSumOfArray.java

--- src/cn/march/algo/array/FindTwoCountSumOfArray.java
+++ src/cn/march/algo/array/FindTwoCountSumOfArray.java
@@ -17,6 +17,10 @@ public class FindTwoCountSumOfArray {
 		
 		findTwoCountSunOfCycl(arrays, arrays.length, 15);
 		
+		System.out.println("-------------------");
+		
+		findTwoCountSunOfOtherArray(arrays, arrays.length, 15);
+		
 	}
 
 	private static void findTwoCountSunOfCycl(int[] arrays, int length, int sum) {
@@ -42,5 +46,50 @@ public class FindTwoCountSumOfArray {
 		}
 
 	}
+	
+	private static void findTwoCountSunOfOtherArray(int[] arrays, int length, int sum) {
+		
+		if(arrays == null || length <= 0)
+			throw new RuntimeException("设置参数不合法!");
+		
+		if(length == 1)
+			if(arrays[0] == sum)
+				System.out
+				.println("arrays数组中：" + arrays[0] + " = sum : " + sum);
 
+		int[] temp_arrays = new int[length];
+		
+		for(int i = 0; i < length; i++) {
+			
+			int sub_v = sum - arrays[i];
+			
+			temp_arrays[i] = sub_v;
+			
+			//System.out.println(temp_arrays[i]);
+			
+		}
+		
+		int begin = 0;
+		int end = length - 1;
+		
+		while(begin <= length - 1 && end >= 0) {		
+			
+			if(arrays[begin] == temp_arrays[end]) {
+							
+				System.out.println("sum 和为arrays数组中的两个元素之和：" + arrays[begin]
+						+ ", " + (sum - arrays[begin]));
+				end--;
+			}
+			end = length - 1;
+			
+			begin++;
+			
+			end--;
+			
+		}
+	
+			
+		
+	}
+	
 }
