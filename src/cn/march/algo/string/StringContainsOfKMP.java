0c8f9afdfce64b6e8b47e532a748dadccd57a794
xiaohuishu <shuxiaohui@foxmail.com>
2015-08-30 18:14:58 +0800

    update cn.march.algo.string.StringContainsOfKMP.java

--- src/cn/march/algo/string/StringContainsOfKMP.java
+++ src/cn/march/algo/string/StringContainsOfKMP.java
@@ -7,7 +7,7 @@ public class StringContainsOfKMP {
 
     public static void main(String [] args) {
 
-        System.out.println(stringContainsOfKMP("shuxiaohui", "xiao1", 10, 5));
+        System.out.println(stringContainsOfKMP("shuxiaohui@gmail.com", "iaohui1", 20, 7));
 
     }
 
@@ -38,26 +38,101 @@ public class StringContainsOfKMP {
 
         boolean flag = true;
 
-        while(i < origin_length && j < contain_length) {
+
+        /**
+         * 传统的移位循环方式
+         */
+        /* while(i < origin_length && j < contain_length) {
 
             if(origin_chars[i] == contain_chars[j]) {
-
                 i++;
                 j++;
                 flag = true;
-
             }else {
-
                 i++;
                 j=0;
+                flag = false;
+            }
+        } */
+
+        int k = 0;
+
+        while (i < origin_length && j < contain_length) {
+
+            if(origin_chars[i] == contain_chars[j]) {
+                i++;
+                j++;
+                k++;
+                flag = true;
+            }else {
+
+                if(k == 0)
+                    i++;
+                else {
+                    System.out.println(k);
+                    int kmp_index = caculKmp_Index(contain_chars, k);
+                    i = i + k - kmp_index;
+                }
+
+                j=0;
+                k=0;
                 flag = false;
 
             }
 
         }
 
+        if(j != contain_length)
+            return false;
+
         return flag;
 
+    }
+
+
+    private static int caculKmp_Index(char[] contain_chars, int k) {
+
+        int index = 0;
+        char [][] fornt_chars = new char[k-1][];
+        char [][] last_chars = new char[k-1][];
+
+        for(int i = 0; i < k-1; i++) {
+
+            fornt_chars[i] = new char[i+1];
+            last_chars[k-i-2] = new char[k-i-1];
+
+            for(int j = 0; j < i+1; j++)
+                fornt_chars[i][j] = contain_chars[j];
+
+            for (int s = i + 1; s < k; s++)
+                last_chars[k-i-2][s-i-1] = contain_chars[s];
+        }
+
+        for(char[] chars : fornt_chars) {
+
+            for(char[] chars_last: last_chars)
+                if(chars == chars_last)
+                    //System.out.println(chars);
+                    index = chars.length;
+
+        }
+
+
+        for(int j = 0; j < fornt_chars.length; j++) {
+
+            System.out.print(String.valueOf(fornt_chars[j]) + " ");
+
+        }
+
+        System.out.println();
+
+        for (int j = 0; j < last_chars.length; j++) {
+
+            System.out.print(String.valueOf(last_chars[j]) + " ");
+
+        }
+
+        return index;
     }
 
 
